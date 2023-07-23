11dbcc285f9736db27ce5a8008d9e9b14b41723c
Aaron Shev <abshev@ucdavis.edu>
2023-07-22 21:42:26 -0700

    complete draft v1

--- _RCode/make_table_data.R
+++ _RCode/make_table_data.R
@@ -6,11 +6,9 @@ makeTableData <- function(plotDat, format = c("percent", "proportion", "dollar")
   nLabels <- length(unique(plotDat$labels))
   cells <- apply(plotDat, 1, format_table_entry, format = format, 
                  digits = digits, confInt = confInt)
+
   outTable <- as.table(matrix(cells, nrow = nLabels, ncol = nYears))
   rownames(outTable) <- unique(plotDat$labels)
   colnames(outTable) <- unique(plotDat$year)
-  if(dim(outTable)[2] > 1){
-    outTable <- outTable[,order(unique(plotDat$year))]
-  }
   return(outTable)
 }
